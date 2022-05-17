e0dfdcf6947dfbf0583a2b94fd378433ce840834
Aizada <91057556+aizadaally@users.noreply.github.com>
2022-05-17 19:41:50 +0600

    Python files

---
+++ Untitled-1.py
@@ -0,0 +1,98 @@
+def get_odd_numbers(numbers):
+    odd_numbers=set()
+    odd_numbers.add
+    for number in numbers:
+        if number % 2 ==1:
+            odd_numbers.add(number)
+    return(odd_numbers)
+
+n= get_odd_numbers([1,2,3,3,4,5,6,7])
+print(n)
+ 
+# def get_unique_list(numbers):
+#     unique_numbers=[]
+#     for x in numbers:
+#         if x is un
+
+
+
+# def numbers():
+#     print(1,2,3,4,5,6)
+# numbers()
+
+# def numbers_divisible_by_2(n):
+#     div_numbers = (n % 2==0)
+#     return div_numbers
+# numbers[1,2,3,4,5,6]
+# numbers_divisible_by_2 = numbers(n)
+# print(numbers_divisible_by_2)
+
+
+# def function(n1,n2):
+#     addition=n1+n2
+#     return addition
+# n1=2
+# n2=2
+# addition= function(n1,n2)
+# print(addition)
+
+# num=([1,2,3,4,4,5,5])
+# num_sum=max(num)
+# print(num_sum)
+
+# def find_average_marks(marks):
+#     sum_of_marks = sum(marks)
+#     total_subject = len(marks)
+#     average_marks = sum_of_marks/total_subject
+#     return average_marks
+# marks = [55,33,2,11,66]
+# average_marks = find_average_marks(marks)
+# print(average_marks)
+
+# def compute_grade (average_marks):
+#     if average_marks>=80:
+#         grade = 'A'
+#     elif average_marks>=60:
+#         grade = 'B'
+#     elif average_marks>=50:
+#         grade = 'C'
+#     else:
+#         grade = 'D'
+#         return grade
+# marks = [12,34,45,0]
+# average_marks= find_average_marks(marks)
+# print(average_marks)
+# grade = compute_grade(average_marks)
+# print ("Average mark is:", grade)
+
+# def div_2(x):
+#     return 3-x
+# a = 3-(6)
+# print(a)
+
+# def numbers():
+#     print(1,2,3,4,45)
+# numbers()
+
+# def numdiv(numbers):
+#     if numbers%2==0:
+#         True
+#     else:
+#         False
+#     return numbers
+# numbers= numdiv(numbers)
+# print (numbers)
+
+
+# import math
+# print (math.pi)
+# from math import pi
+
+
+
+
+    
+    
+
+
+
---
+++ random file.py
@@ -0,0 +1,33 @@
+
+num=3
+if num>=0:
+	print("positive or Zero")
+elif num==0:
+    print ("Zero")
+   
+else:
+	print("Negative number")
+num=-1.4
+if num>0:
+    print("Positive number")
+elif num== 0:
+    print("Zero")
+else:
+    print("Negative number")
+if type (num)==float:
+    print('Float')
+num=float(input("Enter a number:"))
+if num>=0:
+    if num==0:
+        print("Zero")
+    else:
+         print("Positive number")
+else:
+    print("Negative number")
+print("float number")
+
+
+    
+
+    
+
