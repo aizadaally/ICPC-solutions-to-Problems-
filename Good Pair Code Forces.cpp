00c08c435e48bc2af21b57a17896396008db3094
Aizada <91057556+aizadaally@users.noreply.github.com>
2022-05-17 20:07:00 +0600

    Good Pair (from code Forces)

---
+++ Good Pair Code Forces.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//using namespace std;
+//int main () {
+//    int n, m, s;
+//    cout << "";
+//    cin >> n >> m;
+//    s = n + m;
+//    cout << n << "+" << m << "="s;
+//    return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//    int n, s, sum;
+//
+//    cout << " ";
+//    cin >> n >> s;
+//
+//    // sum of two numbers in stored in variable sumOfTwoNumbers
+//    sum = n + s;
+//
+//    // prints sum
+//    cout << n << " + " <<  s << " = " << sum;
+//
+//    return 0;
+//}
+//
+//
+//
