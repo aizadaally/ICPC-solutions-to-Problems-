78fd507904eea0e6d15fd995476574f13e74d2ae
Aizada <91057556+aizadaally@users.noreply.github.com>
2022-05-27 12:51:58 +0600

    Introductory problem

---
+++ Hello.c
@@ -0,0 +1,173 @@
+// #include<stdio.h>
+// int main(){
+//     printf("Helo World!");
+//     return 0;
+//  }
+// // #include<stdio.h>
+// // int main(){
+// //     int a;
+// //     printf("Enter an integer: ");
+// //     scanf("%d, &a");
+// //     if (a<0)
+// //     {
+// //         printf("a is less than 0\n");
+// //     }
+// //     printf("a is less than 0\n");
+// //     return 0;
+// // }
+// // #include<stdio.h>
+// // int main()
+// // {
+// //     int a= 10;
+// //     int b = 10;
+// //     int c= 0;
+// //     if(a==10)
+// //     printf(\n);
+// //     if (b)
+// //     printf("\n");
+// //     if(c)
+// //     printf("\n");
+// //     return 0;
+// // }
+
+
+// // #include <stdio.h>
+// // int main()
+// // {
+// // int a;
+// // printf("English an integer: ");
+// // scanf(%d, &a);
+// // if (a<0)
+// // {
+// //     printf("a is not less than 0\n");
+
+// // }
+// // else
+// // {
+// //     printf("a is not less than 0\n");
+// // }
+// // printf("value of a is : %d\n,a);
+// // return 0;
+// // }
+// #include <stdio.h>
+// int(main)
+// {
+//     int a, b;
+//     scanf(%d %d, &a, &b);
+//     if (a==b)
+// {
+//     printf("a,b");
+// }
+// else if (a>b)
+// {
+//     printf("Result: %d<%d a, b")
+// }
+// else
+// {
+//     printf("Result: %d>%d, a,b");
+// }
+// return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+//     int num = 8;
+//     switch (num){
+//         case 7:
+//         printf("Value is 7 \n");
+//         break;
+//     case 8:
+//     printf("Value is 8\n");
+//         break;
+//     default:
+//         printf("Out of range ");
+//         break;
+
+//     }
+//     return 0;
+// }
+
+  
+//   #include <stdio.h>
+// int main(){
+//     int num = 7;
+//     switch (num){
+//         case 7:
+//         printf("Value is 7 \n");
+        
+//     case 8:
+//     printf("Value is 8\n");
+        
+//     default:
+//         printf("Out of range ");
+        
+
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+// int a = 100;
+// int b = 200;
+// switch (a)
+// {
+//     case 100:
+//         printf("This is part of outer switch :%d\n", a);
+//         switch (b)
+//         {
+//             case 200:
+//             printf("This is part of the inner switch: %d\n", b);
+//         }
+        
+//         break;
+// }
+// printf("Exact value of a is : %d\n", a);
+// printf("Exact value of b is : %d\n", b);
+// return 0;
+
+
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+// printf("   *\n  ***\n *****");
+
+// return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("a = 100 \t b = 100\n");
+//     printf("b = 50004 \t c = 100");
+//     return 0;
+// }
+ 
+//  #include<stdio.h>
+
+// int main(){
+//     printf("int: %d\n", 10);
+//     printf("float: %f \n", 10.0);
+//     printf("char: %c\n", 'c');
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a= 20000000;
+//     long long b = 2*a;
+//     // int b = a*2;
+//     printf(" a:  %d \nb: %lu", a,b );
+//     return 0;
+
+}
+#include<stdio.h>
+#include<bool.h>
+double a = 10;
+float b  = 10.0f;
+printf=("%0.7f, a")
+
